f784e999d0c2f83698125c3869d87b9bfb078f83
Lucas Santana <santanaluc94@gmail.com>
2020-06-06 18:47:09 -0300

    Adicionado frontend menu

--- header.php
+++ header.php
@@ -9,12 +9,13 @@
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="<?php bloginfo("template_directory"); ?>/css/reset.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo("template_directory"); ?>/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>
         <?php if (!empty(get_bloginfo('name'))) : ?>
             <?= get_bloginfo('name') ?>
         <?php else : ?>
-            <?= __('Página Inicial', 'header') ?>
+            Página Inicial
         <?php endif; ?>
     </title>
 </head>
@@ -24,30 +25,51 @@
         <nav>
             <div class="menu-barra-superior">
                 <!-- Escudo do Time -->
-                <a href="<?= get_home_url() ?>">
-                    <?php if (function_exists('the_custom_logo')) : ?>
-                        <?php the_custom_logo() ?>
-                    <?php else : ?>
-                        <img src="<?= get_home_url() . '/img/adc-logo.png' ?>" />
-                    <?php endif; ?>
-                </a>
+                <?php if (has_custom_logo()) : ?>
+                    <?php the_custom_logo() ?>
+                <?php else : ?>
+                    <a class="logo" href="<?= site_url() ?>">
+                        <img class="logo-menu" src="<?= bloginfo("template_directory") . '/img/adc-logo.png' ?>" />
+                    </a>
+                <?php endif; ?>
 
                 <div class="titulo">
                     <!-- Nome do Clube -->
-                    <h2 class="titulo_nome">
+                    <h2 class="titulo">
                         <?php if (get_bloginfo('name') === null) : ?>
                             <?= get_bloginfo('name') ?>
                         <?php else : ?>
                             Associação Desportiva Clube
                         <?php endif; ?>
                     </h2>
-                    <h3 class="titulo_site">Site Oficial</h3>
+                    <h3 class="titulo">Site Oficial</h3>
+                </div>
+
+                <div>
+                    <ul class="midias-sociais-superior">
+                        <li class="list-inline-item">
+                            <a href="https://www.facebook.com/GuarulhosGRU/" class="fa-superior fa fa-facebook-f"></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://twitter.com/GuarulhosGRU" class="fa-superior fa fa-twitter"></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://www.mercadolivre.com.br/GuarulhosGRU" class="fa-superior fa fa-handshake-o"></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://www.instagram.com/guarulhosgru/" class="fa-superior fa fa-instagram"></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://www.youtube.com/channel/UCduGenvRYg0KopQiJUvzbVQ/videos" class="fa-superior fa fa-youtube-play"></a>
+                        </li>
+                    </ul>
                 </div>
             </div>
 
             <div class="menu-barra-inferior">
                 <div class="barra_navegacao botoes">
-                    <?php wp_nav_menu(array('theme_location' => 'header_menu')); ?>
+                    <!-- Sobre o Clube, Elenco, Jogos, Extras, Seja sócio -->
+                    <?php wp_nav_menu(['theme_location' => 'header_menu']); ?>
                 </div>
             </div>
         </nav>
