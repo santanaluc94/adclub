98e78ad0c7b024a1108a3d74f054f45159f3d6d4
Lucas Santana <lsantana@itelios.com>
2020-06-10 23:10:09 -0300

    Arrumando página de Atletas

--- page-campeonatos.php
+++ page-campeonatos.php
@@ -1,60 +1,35 @@
 <?php
 
 /**
- * template name: atletas
+ * template name: campeonatos
  */
 
 get_header();
+
+query_posts([
+    'post_type' => ['campeonatos'],
+    'posts_per_page' => -1,
+    'orderby' => 'posicao',
+    'order' => 'asc'
+]);
 ?>
-<div class="container container-top d-flex">
-    <div class="conteudo-flex col-md-12">
-        <section class="secao">
-            <div class="row titulo-site">
-                <h1>
-                    <span class="titulo-pagina"><?php the_title(); ?></span>
-                </h1>
-            </div>
-            <div class="card-deck">
-                <?php query_posts(array(
-                    'post_type' => array('atletas'),
-                    'posts_per_page' => -1,
-                    'orderby' => 'posicao',
-                    'order' => 'asc'
-                ));
-                ?>
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                        <div class="col-md-3">
-                            <div class="card d-flex">
-                                <div class="image">
-                                    <?php the_post_thumbnail('thumbnail', ['class' => 'card-img-top text-center']) ?>
-                                </div>
-                                <div class="card-footer">
-                                    <h5 class="card-title"><?php the_field('nome'); ?></h5>
-                                    <p class="card-text">
-                                        <span>Nome:</span> <?php the_field('nome_completo'); ?>
-                                    </p>
-                                    <p class="card-text">
-                                        <span>Peso:</span> <?php the_field('peso'); ?>kg
-                                    </p>
-                                    <p class="card-text">
-                                        <span> Altura:</span> <?php the_field('altura'); ?>cm
-                                    </p>
-                                    <p class="card-text">
-                                        <span>Nascimento:</span> <?php the_field('data_de_nascimento'); ?>
-                                    </p>
-                                    <p class="card-text">
-                                        <span>Posição:</span> <?php the_field('posicao'); ?>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endwhile; ?>
-                <?php else : ?>
-                    Não há atletas cadastrados
-                <?php endif; ?>
-                <?php wp_reset_query(); ?>
-            </div>
-        </section>
-    </div>
+<div class="container container-top">
+    <section class="inicio-secao">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="row titulo-site">
+                    <h1>
+                        <span class="titulo-pagina"><?php the_title(); ?></span>
+                    </h1>
+                </div>
+                <article class="sub-texto">
+
+                </article>
+            <?php endwhile; ?>
+        <?php else : ?>
+            O clube não está em campeonatos no momento.
+        <?php endif; ?>
+        <?php wp_reset_query(); ?>
+    </section>
 </div>
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -11,29 +11,26 @@ get_header();
     <section class="inicio-secao">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-                <div class="conteudo-flex">
-                    <div class="row titulo-site">
-                        <h1>
-                            <span class="titulo-pagina"><?php the_title(); ?></span>
-                        </h1>
-                    </div>
-                    <article class="texto">
-                        <?php if (has_post_thumbnail()) : ?>
-                            <div class="imagem-superior img-responsive">
-                                <?php the_post_thumbnail('post_thumbnail', ['class' => 'imagem-superior img-responsive']); ?>
-                            </div>
-                        <?php endif; ?>
-                        <?php if (the_content()) : ?>
-                            <p class="texto-pagina">
-                                <?php the_content(); ?>
-                            </p>
-                        <?php endif; ?>
-                    </article>
-                    <?php comments_template(); ?>
-
-                    <?php comments_popup_link('Sem Comentários', '1 Comentário', '% Comentários', 'comments-link', ''); ?>
+                <div class="row titulo-site">
+                    <h1>
+                        <span class="titulo-pagina"><?php the_title(); ?></span>
+                    </h1>
                 </div>
+                <article class="sub-texto">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <div class="imagem-superior img-responsive">
+                            <?php the_post_thumbnail('post_thumbnail', ['class' => 'imagem-superior img-responsive']); ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php if (the_content()) : ?>
+                        <p class="texto-pagina">
+                            <?php the_content(); ?>
+                        </p>
+                    <?php endif; ?>
+                </article>
+                <?php comments_template(); ?>
 
+                <?php comments_popup_link('Sem Comentários', '1 Comentário', '% Comentários', 'comments-link', ''); ?>
             <?php endwhile; ?>
         <?php else : ?>
             <div class="artigo">
