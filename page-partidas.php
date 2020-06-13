1878abd0e8ca3572e99ccfe199a99a6ba691b26c
Lucas Santana <santanaluc94@gmail.com>
2020-06-13 18:12:56 -0300

    página de partidas para desk

--- page-partidas.php
+++ page-partidas.php
@@ -3,66 +3,73 @@
 /**
  * template name: partidas
  */
-get_header();
 
 query_posts([
     'post_type' => ['partidas'],
     'posts_per_page' => -1
 ]);
 
+$campeonatos = get_terms('campeonatos');
+
 get_header(); ?>
 
 ?>
-<div class="container d-flex">
-    <div class="conteudo-flex col-md-9 col-sm-12">
-        <section class="inicio-secao">
-            <div class="row titulo-site">
-                <h1>
-                    <span class="titulo-pagina"><?php the_title(); ?></span>
-                </h1>
-            </div>
-        </section>
-        <section class="inicio-secao">
-            <div class="col-12 colunas-secao d-flex align-items-center">
-                <div id="" class="col-12">
+<div class="container container-top">
+    <section class="inicio-secao">
+        <div class="row titulo-site">
+            <h1>
+                <span class="titulo-pagina"><?php the_title(); ?></span>
+            </h1>
+        </div>
+    </section>
+    <section class="secao">
+        <div class="colunas-secao d-flex align-items-center">
+            <div class="col-md-12">
+                <?php foreach ($campeonatos as $campeonato) : ?>
+                    <div class="row conteudo-partida">
+                        <h2>
+                            <span class="titulo-campeonato"><?= $campeonato->name ?></span>
+                        </h2>
+                    </div>
                     <?php if (have_posts()) : ?>
-                        <?php while (have_posts()) : the_post(); ?>
-                            <?php $campeonato = get_the_term_list(get_the_ID(), 'campeonatos'); ?>
-                            <div class="row titulo-site">
-                                <h1>
-                                    <span class="titulo-pagina"><?= $campeonato ?></span>
-                                </h1>
-                            </div>
-                            <div class="card">
-                                <div class="card-header">
-                                    <a class="btn btn-link" style="width:100%">
+                        <div class="row">
+                            <?php while (have_posts()) : the_post(); ?>
+
+
+                                <div class="col-md-5 card">
+                                    <div class="card-header">
+
+
                                         <div class="d-flex">
                                             <div class="placar">
-                                                <img class="escudo" src="<?= get_post_meta(get_the_ID(), '')[0]; ?>" />
-                                                <span class="texto-card"><?= get_post_meta(get_the_ID(), 'mandante')[0];; ?></span>
+                                                <img class="escudo" src="<?= bloginfo("template_directory") . '/img/adc-logo.png' ?>" />
+                                                <span class="texto-card" style=><?= substr(get_post_meta(get_the_ID(), 'mandante')[0], 0, 3); ?></span>
                                                 <span class="texto-card-info"><?= get_post_meta(get_the_ID(), 'estadio')[0]; ?></span>
                                             </div>
                                             <div class="placar">
-                                                <span class="texto-card-placar"><?= get_post_meta(get_the_ID(), '')[0]; ?> x <?= get_post_meta(get_the_ID(), '')[0]; ?></span>
+                                                <span class="texto-card-placar">1 x 1</span>
                                             </div>
                                             <div class="placar">
-                                                <img class="escudo" src="<?= get_post_meta(get_the_ID(), '')[0];; ?>" />
-                                                <span class="texto-card"><?= get_post_meta(get_the_ID(), 'visitante')[0]; ?></span>
-                                                <span class="texto-card-info"><?= get_post_meta(get_the_ID(), 'horario')[0]; ?></span>
+                                                <img class="escudo" src="<?= bloginfo("template_directory") . '/img/adc-logo.png' ?>" />
+                                                <span class="texto-card"><?= substr(get_post_meta(get_the_ID(), 'visitante')[0], 0, 3); ?></span>
+                                                <span class="texto-card-info">
+                                                    <?= get_post_meta(get_the_ID(), 'data')[0]; ?> <?= get_post_meta(get_the_ID(), 'horario')[0]; ?>
+                                                </span>
                                             </div>
                                         </div>
 
-                                    </a>
+                                    </div>
                                 </div>
-                            </div>
-                        <?php endwhile; ?>
+
+                            <?php endwhile; ?>
+                        </div>
                     <?php else : ?>
                         Não há partidas cadastradas.
                     <?php endif; ?>
                     <?php wp_reset_query(); ?>
-                </div>
+                <?php endforeach; ?>
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
 </div>
 <?php get_footer(); ?>
